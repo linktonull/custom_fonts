b207fb7b9bc76f0284e818bf4484c89319527378
linktonull <183869923+linktonull@users.noreply.github.com>
2025-06-20 17:23:41 +0000

    init

--- ttf/1.sh
+++ ttf/1.sh
@@ -3,9 +3,9 @@ mkdir -p otd
 chmod 777 otd
 ./bin/otfccdump ttf/YsabeauInfant_Italic.ttf -o otd/base.otd
 ./bin/otfccdump ttf/LXGWWenKai-Regular.ttf -o otd/1.otd
-./bin/otfccdump ttf/NotoSansSC-Regular.otf -o otd/2.otd
-./bin/otfccdump ttf/NotoSansTC-Regular.otf -o otd/3.otd
-./bin/otfccdump ttf/NotoSansKR-Regular.otf -o otd/4.otd
-./bin/otfccdump ttf/NotoSansJP-Regular.otf -o otd/5.otd
+./bin/otfccdump ttf/NotoSansCJKsc-VF.ttf -o otd/2.otd
+./bin/otfccdump ttf/NotoSansCJKtc-VF.ttf -o otd/3.otd
+./bin/otfccdump ttf/NotoSansCJKkr-VF.ttf -o otd/4.otd
+./bin/otfccdump ttf/NotoSansCJKjp-VF.ttf -o otd/5.otd
 ./bin/otfccdump ttf/Arimo-Italic.ttf -o otd/6.otd
 tree
